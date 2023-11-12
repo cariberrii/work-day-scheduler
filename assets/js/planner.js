ed84d510cd1a6137d64a25bf574e68501dd03edf
cariberrii <ca.fillingham@gmail.com>
2023-11-12 20:05:09 +0000

    Inputs save to localStorage and persist upon page refresh

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -4,27 +4,27 @@ console.log("This is planner.js");
 var dateToday = dayjs().format("dddd[,] MMMM D");
 $("#currentDay").text(dateToday);
 
- 
+
 // Limit rows of text to 3 within each hour input
 $('textarea[data-limit-rows=true]').on('keypress', function (event) {
     var textarea = $(this),
         numberOfLines = (textarea.val().match(/\n/g) || []).length + 1,
         maxRows = parseInt(textarea.attr('rows'));
-    
-    if (event.which === 13 && numberOfLines === maxRows ) {
-      return false;
-    }
-  });
 
-  
-  // Change color of hour block based on current time
-  function hourColor() {
-      var hour = parseInt(dayjs().format("H"));
-      console.log(hour);
-      
-      $(".input-hour").each(function() {
-          var currentHour = parseInt($(this).attr("id"));
-          
+    if (event.which === 13 && numberOfLines === maxRows) {
+        return false;
+    }
+});
+
+
+// Change color of hour block based on current time
+function hourColor() {
+    var hour = parseInt(dayjs().format("H"));
+    console.log(hour);
+
+    $(".input-hour").each(function () {
+        var currentHour = parseInt($(this).attr("id"));
+
         console.log(currentHour);
         console.log(this);
 
@@ -38,5 +38,37 @@ $('textarea[data-limit-rows=true]').on('keypress', function (event) {
     })
 };
 
-// Call function
+// Save tasks
+// var taskInput = $("textarea").val();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function() {
+    console.log(this);
+
+    var time = $(this).siblings(".hour").text();
+    var task = $(this).siblings(".task").val();
+
+    console.log(time);
+    console.log(task);
+
+    localStorage.setItem(time, task);
+});
+
+
+// Render saved schedule
+function renderSchedule() {
+    $(".hour").each(function() {
+        var currentHour = $(this).text();
+        var currentTask = localStorage.getItem(currentHour);
+
+        console.log(currentHour);
+        console.log(currentTask);
+
+        if (currentTask !== null) {
+            $(this).siblings(".task").val(currentTask);
+        }
+    })
+}
+// Call functions
 hourColor();
+renderSchedule();
